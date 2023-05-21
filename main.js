45d99d384019aae0be7f696f1e438f968efc70c0
shemshajili <shamsah@code.edu.az>
2023-05-21 23:21:37 +0400

    task is done:))

---
+++ main.js
@@ -0,0 +1,14 @@
+// const testimonialsLink = document.querySelector(".testimonial-link");
+// testimonialsLink.addEventListener("click", scrollToTestimonials);
+
+// function scrollToTestimonials(event) {
+//     event.preventDefault();
+//     const testimonialsSection = document.querySelector(".carousel");
+//     const topOffset =
+//       testimonialsSection.offsetTop -
+//       headerContainer.offsetHeight -
+//       headerBotRowContainer.offsetHeight;
+//     window.scrollTo({ top: topOffset, behavior: "smooth" });
+//   }
+//   const clientsLink = document.querySelector(".clients-link");
+//   clientsLink.addEventListener("click", scrollToClients);
